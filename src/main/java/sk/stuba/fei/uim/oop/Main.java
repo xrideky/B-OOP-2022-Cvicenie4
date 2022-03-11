700b9e14454dac4abf5b14ee56a5b43b679187f1
xrideky <xrideky@stuba.sk>
2022-03-11 17:42:54 +0100

    kolekcie

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,16 +1,55 @@
 package sk.stuba.fei.uim.oop;
 
+
 import sk.stuba.fei.uim.oop.zvierata.Macka;
 import sk.stuba.fei.uim.oop.zvierata.Pes;
+import sk.stuba.fei.uim.oop.zvierata.Zviera;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
-        //nemozeme spravit napriklad var zviera=new Zviera(); pretoze to je abstraktna trieda
-        var dunco=new Pes("Dunco");
-        var garfield=new Macka("Garfield");
 
-        dunco.pohladkat();
-        garfield.pohladkat();
+        //var list=new ArrayList<>();
+        ArrayList<Pes> list = new ArrayList<Pes>();
+        list.add(new Pes("dunco"));
+        list.add(new Pes("luna"));
+        System.out.println(list.size());
+        /*list.remove(0);
+        //list nie je prazdny ale ked dam este raz list.remove(0) tak uz prazdny bude
+        System.out.println(list.isEmpty());*/
 
+
+        ArrayList<Macka> listMaciek = new ArrayList<Macka>();
+        listMaciek.add(new Macka("garfield"));
+        listMaciek.add(new Macka("murko"));
+        System.out.println(listMaciek.size());
+
+        ArrayList<Zviera> listZveri = new ArrayList<>();
+        listZveri.addAll(list);
+        listZveri.addAll(listMaciek);
+        System.out.println(listZveri.size());
+
+        // "prekartanie veci"
+        Collections.shuffle(listZveri);
+
+        //listZveri.get(0).pohladkat();
+
+        for (var i = 0; i < listZveri.size(); i++) {
+            //pretypovanie ked si neurcim akeho typu je dany list
+            ((Zviera)listZveri.get(i)).pohladkat();
+        }
+
+        for(var zver : listZveri){
+            zver.pohladkat();
+        }
+
+        var iterator = listZveri.iterator();
+        while(iterator.hasNext()){
+            var prvok=iterator.next();
+             prvok.zvukZvierata();
+        }
     }
 }
