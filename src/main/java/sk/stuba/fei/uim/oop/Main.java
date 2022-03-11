cc227f7f8eaea5d2d83a2f98f081a74cb861330c
xrideky <xrideky@stuba.sk>
2022-03-11 14:33:54 +0100

    ukazka protected modifikatoru

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -2,6 +2,10 @@ package sk.stuba.fei.uim.oop;
 
 public class Main {
     public static void main(String[] args) {
-
+        Test test=new Test();
+        test.jeden=1;
+        test.dva=2;
+        test.tri=3;
+        //viem pristupovat aj k protected veciam
     }
 }
