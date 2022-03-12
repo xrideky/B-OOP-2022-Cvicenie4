04695ee178ecd63ac53689aed487197098157551
xrideky <xrideky@stuba.sk>
2022-03-12 18:02:54 +0100

    praca s kolekciami

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,55 +1,32 @@
 package sk.stuba.fei.uim.oop;
 
-
-import sk.stuba.fei.uim.oop.zvierata.Macka;
 import sk.stuba.fei.uim.oop.zvierata.Pes;
 import sk.stuba.fei.uim.oop.zvierata.Zviera;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) {
 
-        //var list=new ArrayList<>();
         ArrayList<Pes> list = new ArrayList<Pes>();
         list.add(new Pes("dunco"));
         list.add(new Pes("luna"));
         System.out.println(list.size());
-        /*list.remove(0);
-        //list nie je prazdny ale ked dam este raz list.remove(0) tak uz prazdny bude
-        System.out.println(list.isEmpty());*/
 
-
-        ArrayList<Macka> listMaciek = new ArrayList<Macka>();
-        listMaciek.add(new Macka("garfield"));
-        listMaciek.add(new Macka("murko"));
-        System.out.println(listMaciek.size());
-
-        ArrayList<Zviera> listZveri = new ArrayList<>();
-        listZveri.addAll(list);
-        listZveri.addAll(listMaciek);
-        System.out.println(listZveri.size());
-
-        // "prekartanie veci"
-        Collections.shuffle(listZveri);
-
-        //listZveri.get(0).pohladkat();
-
-        for (var i = 0; i < listZveri.size(); i++) {
-            //pretypovanie ked si neurcim akeho typu je dany list
-            ((Zviera)listZveri.get(i)).pohladkat();
+        //ArrayList to pole
+        Pes[] pole=new Pes[2];
+        list.toArray(pole);
+        for(Pes zver:pole){
+            zver.zvukZvierata();
         }
 
-        for(var zver : listZveri){
-            zver.pohladkat();
-        }
-
-        var iterator = listZveri.iterator();
-        while(iterator.hasNext()){
-            var prvok=iterator.next();
-             prvok.zvukZvierata();
-        }
+        //pole to ArrayList
+        List<Pes> psi = Arrays.asList(pole);
+        ArrayList<Pes> list2=new ArrayList<>(psi);
+        list2.add(new Pes("dunco 2"));
+        System.out.println(list2.size());
     }
 }
